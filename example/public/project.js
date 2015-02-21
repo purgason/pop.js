af68677992b9714d019e18109126467d7aac9c8a
purgason <dpurgy@hotmail.com>
2015-02-20 20:17:31 -0600

    Update and rename example/public to example/public/project.js

---
+++ example/public/project.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $("#defaultdemo").click(function() {
+        simplePopup({
+            'pop-title':'Pop Demo', 
+            'pop-body':'This is a demonstration of a simple popup. The point is to present information to the user and provide a way to dismiss the popup.', 
+            'btn-text':'Dismiss',
+            'auto-break': 25,
+        });
+    });
+    $("#customdemo").click(function() {
+        simplePopup({
+            'shrink-in':true,
+            'slide-in':true,
+            'slide-out':true,
+            'btn-style':'success', 
+            'round-corners':true, 
+            'head-align':'center', 
+            'body-align':'center', 
+            'foot-align':'right', 
+            'header-bg-shade':true,
+            'footer-bg-shade':false,
+            'dragable':true, 
+            'auto-break': 25,
+            'pop-title':'Pop Demo', 
+            'pop-body':'This is a demonstration of a customized popup. Notice the differences in style and presentation.', 
+            'btn-text':'Dismiss',
+            'click-fn':sampleExitTask
+        });
+    });
+});
+
+function sampleExitTask() {
+    $("#exitTaskOutput").html("Action on Popup Dismissal.<br>This event fired at the time the popup was dismissed based on the function assigned to the 'click-fn' option.");
+    $("#exitTaskOutput").css({padding: "4px", background: "#ff8080"});
+    $("#exitTaskOutput").fadeIn();
+    setTimeout(function(){
+        $("#exitTaskOutput").fadeOut();
+    },6000);
+}
